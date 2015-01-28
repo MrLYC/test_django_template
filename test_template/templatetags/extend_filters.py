2b10776aeefbbd4647de704a49956d0ff40d6ff3
MrLYC <imyikong@gmail.com>
2015-01-28 17:20:09 +0800

    fix random_choice

--- test_template/templatetags/extend_filters.py
+++ test_template/templatetags/extend_filters.py
@@ -6,6 +6,14 @@ from django import template
 register = template.Library()
 
 
+@register.filter(name="random_choice")
+def random_choice(*args):
+    """Return random.choice(args)
+    """
+    import random
+    return random.choice(args)
+
+
 @register.filter(name="type")
 def type_(value):
     """Return type(value)
@@ -21,3 +29,31 @@ def oruser(name):
         return name
     import getpass
     return getpass.getuser()
+
+
+@register.filter(name="getattr")
+def getattr(obj, attr):
+    """Return getattr(obj, attr)
+    """
+    return getattr(obj, attr)
+
+
+@register.filter(name="hasattr")
+def hasattr(obj, attr):
+    """Return hasattr(obj, attr)
+    """
+    return hasattr(obj, attr)
+
+
+@register.filter(name="getkey")
+def getkey(dict_, key, default=None):
+    """Return dict_.get(key, default)
+    """
+    return dict_.get(key, default)
+
+
+@register.filter(name="haskey")
+def haskey(dict_, key):
+    """Return dict_.haskey(key))
+    """
+    return dict_.haskey(key)
